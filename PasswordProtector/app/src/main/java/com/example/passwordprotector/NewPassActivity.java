383db4991536b22f020ba1496836af47a64e359b
chrisgang311 <christopher.gang@yahoo.com>
2017-04-30 16:51:07 -0500

    cmmnnt

--- PasswordProtector/app/src/main/java/com/example/passwordprotector/NewPassActivity.java
+++ PasswordProtector/app/src/main/java/com/example/passwordprotector/NewPassActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.passwordprotector;
 
 import android.content.Intent;
@@ -61,8 +62,9 @@ public class NewPassActivity extends AppCompatActivity {
             }
         });
     }
-    
+    //called whenever save button is clicked
     public void saveButtonClicked(View view){
+        //getting values from ui user input
         String name =  accountNameText.getText().toString();
         boolean specialChars = specialCharBox.isChecked();
         boolean capLetters = capLettersBox.isChecked();
@@ -71,38 +73,42 @@ public class NewPassActivity extends AppCompatActivity {
         String q1="";
         String q2="";
         String q3="";
+        //sets security answers if checkbox is clicked
         if(secQ){
             q1 = securityQ1.getText().toString();
             q2 = securityQ2.getText().toString();
             q3 = securityQ3.getText().toString();
         }
         int numLetters=0;
+        //tries to get number input for password length
         try{
             String nums = numberCharText.getText().toString();
             numLetters = Integer.parseInt(nums);
-        }catch(Exception e){
+        }catch(Exception e){//catches exception for invalid entry
             Toast.makeText(NewPassActivity.this,"Please insert a valid number", Toast.LENGTH_SHORT).show();
         }
-
+        //object for password generation
         PasswordOnly passwordOnly = new PasswordOnly();
+        //object for database handling
         DBHandler dbHandler = DBHandler.getInstance(this);
+        //checks to verify that an account name was input
         if(name.equals("")){
             Toast.makeText(NewPassActivity.this,"Please name the account.", Toast.LENGTH_SHORT).show();
-        }else if(numLetters < 8){
+        }else if(numLetters < 8){//checking if the password length is within range
             Toast.makeText(NewPassActivity.this,"Please set password length to greater than 8", Toast.LENGTH_SHORT).show();
         }else if(numLetters > 32){
             Toast.makeText(NewPassActivity.this,"Please set password length to less than 32", Toast.LENGTH_SHORT).show();
-        }
+        }//checks to see if the name of account is already used
         else if(dbHandler.nameInDatabase(passphrase,name)){
             //add a Toast that lets the user know that the account name is taken, and that they need to enter a different one
             Toast.makeText(NewPassActivity.this, "The username -" + name + "- is taken, please enter a different username.",Toast.LENGTH_SHORT).show();
-        }else{
-            Password password = passwordOnly.addPassword(name,numLetters,capLetters,specialChars,numbers);
+        }else{//we gucci for account creation
+            Password password = passwordOnly.addPassword(name,numLetters,capLetters,specialChars,numbers);//gets random password with input params
             password.setSecurityAnswers(q1);
             password.setSecurityAnswers(q2);
             password.setSecurityAnswers(q3);
-            dbHandler.addAccount(password, passphrase);
-            finish();
+            dbHandler.addAccount(password, passphrase);//adds account
+            finish();//returns to main activity
         }
 
 
